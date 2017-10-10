0f987cc7d20ea97babb5b8b91dd94ae66154c6b1
Jacob H. Clausen <hijakker123@hotmail.com>
2017-10-10 21:41:10 +0200

    Debugging trouble with json object preventing sending response

--- ServerAssignment/EchoServer.cs
+++ ServerAssignment/EchoServer.cs
@@ -4,19 +4,25 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace ServerAssignment
 {
     class EchoServer
     {
-        TcpListener server = null;
+        private List<Category> categories = null;
+            TcpListener server = null;
         public EchoServer()
         {
             // List of the categories. Now they are loaded in memory, might be better with a static class.
-            List<Category> categories = new List<Category>();
-            categories.Add(new Category(1, "Beverages"));
-            categories.Add(new Category(2, "Condiments"));
-            categories.Add(new Category(3, "Confections"));
+            categories = new List<Category>
+            {
+                new Category(1, "Beverages"),
+                new Category(2,"Condiments"),
+                new Category(3,"Confections")
+            };
+
 
             // Server setup
             int port = 5000;
@@ -45,23 +51,91 @@ namespace ServerAssignment
             var client = clientObj as TcpClient;
             if (client == null) return;
 
-            var strm = client.GetStream();
+            try
+            {
+                var strm = client.GetStream();
 
-            byte[] buffer = new byte[client.ReceiveBufferSize];
-            int bytesRead = strm.Read(buffer, 0, buffer.Length);
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                var bytesRead = strm.Read(buffer, 0, buffer.Length);
 
-            var request = Encoding.UTF8.GetString(buffer);
+                var request = Encoding.UTF8.GetString(buffer);
+                request = request.Trim('\0');
+                Console.WriteLine(request);
+
+                //var responseObject = new
+                //{
+                //    test = "test"
+                //};
+
+                //var response = Encoding.UTF8.GetBytes(responseObject.ToJson());
+
+                //Console.WriteLine(response.ToJson());
+
+                //strm.Write(response, 0, response.Length);
+
+                /*var jsonObject = JsonConvert.DeserializeObject<Request>(request);
+                Console.WriteLine(jsonObject.date);
+                var json = jsonObject.ToJson();
+                var jsonBytes = Encoding.UTF8.GetBytes(json);
+                strm.Write(jsonBytes, 0, jsonBytes.Length);*/
+
+                var categories = new List<object>
+                {
+                    new {cid = 1, name = "Beverages"},
+                    new {cid = 2, name = "Condiments"},
+                    new {cid = 3, name = "Confections"}
+                };
+
+                var response = new
+                {
+                    Status = "1 Ok",
+                    Body = categories.ToJson()
+                };
+
+                var responseJson = response.ToJson();
+
+                var responseBytes = Encoding.UTF8.GetBytes(responseJson);
+                strm.Write(responseBytes, 0, responseBytes.Length);
+
+                //Console.WriteLine(jsonObject.method);
+
+                //var response = categories.ToJson();
+
+                //var msg = Encoding.UTF8.GetBytes(response);
+                //strm.Write(msg, 0, msg.Length);
+
+                //var response = Encoding.UTF8.GetBytes(request.ToUpper());
+
+                //strm.Write(response, 0, bytesRead);
+
+                strm.Close();
+
+                client.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
             
-            Console.WriteLine(request.Trim('\0'));
+        }
+    }
 
-            var response = Encoding.UTF8.GetBytes(request.ToUpper());
+    public class Request
+    {
+        public string method { get; set; }
+        //public string Body;
+        public string path { get; set; }
 
-            strm.Write(response, 0, bytesRead);
+        public string date { get; set; }
+    }
 
-            strm.Close();
-
-            client.Dispose();
+    public static class Util
+    {
+        public static string ToJson(this object data)
+        {
+            return JsonConvert.SerializeObject(data,
+                new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()});
         }
     }
 }
